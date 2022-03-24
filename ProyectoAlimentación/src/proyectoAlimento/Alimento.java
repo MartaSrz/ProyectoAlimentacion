90d285ff51b9e9f0a451ca14171259cdd89a6190
Marta <m.s.m.2003.marta@gmail.com>
2022-03-24 11:40:49 +0100

    Proyecto
    Esta es el proyecto recién creado
    
    
    Signed-off-by: Marta <m.s.m.2003.marta@gmail.com>

---
+++ ProyectoAlimentación/src/proyectoAlimento/Alimento.java
@@ -0,0 +1,43 @@
+package proyectoAlimento;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Alimento {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Alimento window = new Alimento();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Alimento() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
