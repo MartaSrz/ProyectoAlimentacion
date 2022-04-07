03f17ab35f7f98ee48b7fa57eaeb66e69e177819
marmar03 <marmar03@client>
2022-04-07 09:20:40 +0200

    Mensajes de error
    He añadido mensajes de error correspondientes si el usuario no
    especifica ni su sexo ni su nombre, lo que va a llevar a no pasar a la
    siguiente ventana hasta que los ponga.
    
    
    
    Signed-off-by: marmar03 <marmar03@client>

--- ProyectoAlimentación/src/proyectoAlimento/DatosUsuario.java
+++ ProyectoAlimentación/src/proyectoAlimento/DatosUsuario.java
@@ -30,6 +30,7 @@ public class DatosUsuario{
 	private JTextField textField;
 	private final Color FONDO_COLOR_TITULO = new Color(255,255,221);
 	private final Color FONDO_COLOR = new Color(236,224,251);
+	private final String DEFECTO_TXTNOMBRE="Escribe tu nombre...";
 
 
 	/**
@@ -83,19 +84,12 @@ public class DatosUsuario{
 		frame.getContentPane().add(lblComotellamas);
 
 		txtNombre = new JTextField();
-		txtNombre.setText("Escribe tu nombre...");
-		txtNombre.setFont(new Font("Dialog", Font.PLAIN, 16));
+		txtNombre.setText(DEFECTO_TXTNOMBRE);
+		txtNombre.setFont(new Font("Dialog", Font.PLAIN, 14));
 		txtNombre.setForeground(Color.GRAY);
-		txtNombre.setBounds(265, 123, 666, 25);
+		txtNombre.setBounds(265, 125, 666, 25);
 		frame.getContentPane().add(txtNombre);
 		txtNombre.setColumns(10);
-		txtNombre.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				txtNombre.setText("");
-				txtNombre.setForeground(Color.BLACK);
-			}
-		});
 
 		JLabel lblSexo = new JLabel("Sexo:");
 		lblSexo.setForeground(Color.BLACK);
@@ -128,7 +122,7 @@ public class DatosUsuario{
 		JLabel lblEdad = new JLabel("Edad:");
 		lblEdad.setForeground(Color.BLACK);
 		lblEdad.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblEdad.setBounds(121, 315, 60, 17);
+		lblEdad.setBounds(121, 335, 60, 17);
 		frame.getContentPane().add(lblEdad);
 
 		JSpinner spinner_anyos = new JSpinner();
@@ -137,7 +131,7 @@ public class DatosUsuario{
 		spinner_anyos.setModel(new SpinnerNumberModel(new Integer(18), new Integer(0), null, new Integer(1)));
 		spinner_anyos.setValue(18);
 		spinner_anyos.setFont(new Font("Dialog", Font.PLAIN, 16));
-		spinner_anyos.setBounds(165, 342, 91, 22);
+		spinner_anyos.setBounds(165, 362, 91, 22);
 		frame.getContentPane().add(spinner_anyos);
 
 		JLabel lblBienvenido = new JLabel("¡Bienvenido a nuestra aplicación! ");
@@ -156,46 +150,72 @@ public class DatosUsuario{
 
 		JLabel lblAnyos = new JLabel("años");
 		lblAnyos.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblAnyos.setBounds(265, 345, 60, 17);
+		lblAnyos.setBounds(265, 365, 60, 17);
 		frame.getContentPane().add(lblAnyos);
 
 		JLabel lblPeso = new JLabel("Peso:");
 		lblPeso.setForeground(Color.BLACK);
 		lblPeso.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblPeso.setBounds(745, 203, 60, 17);
+		lblPeso.setBounds(690, 203, 60, 17);
 		frame.getContentPane().add(lblPeso);
 
 		JLabel lblKg = new JLabel("kg");
 		lblKg.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblKg.setBounds(907, 225, 19, 31);
+		lblKg.setBounds(852, 225, 19, 31);
 		frame.getContentPane().add(lblKg);
 
 		JSpinner spinner_peso = new JSpinner();
 		spinner_peso.setModel(new SpinnerNumberModel(new Integer(70), new Integer(0), null, new Integer(1)));
 		spinner_peso.setValue(70);
 		spinner_peso.setFont(new Font("Dialog", Font.PLAIN, 16));
-		spinner_peso.setBounds(802, 229, 96, 22);
+		spinner_peso.setBounds(747, 229, 96, 22);
 		frame.getContentPane().add(spinner_peso);
 
 		JLabel lblAltura = new JLabel("Altura:");
 		lblAltura.setForeground(Color.BLACK);
 		lblAltura.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblAltura.setBounds(745, 315, 60, 17);
+		lblAltura.setBounds(690, 335, 60, 17);
 		frame.getContentPane().add(lblAltura);
 
 		JSpinner spinner_altura = new JSpinner();		
 		spinner_altura.setModel(new SpinnerNumberModel(new Integer(160), new Integer(0), null, new Integer(1)));
 		spinner_altura.setValue(160);
 		spinner_altura.setFont(new Font("Dialog", Font.PLAIN, 16));
-		spinner_altura.setBounds(802, 342, 96, 22);
+		spinner_altura.setBounds(747, 362, 96, 22);
 		frame.getContentPane().add(spinner_altura);
 
 		JLabel lblcm = new JLabel("cm");
 		lblcm.setFont(new Font("Dialog", Font.PLAIN, 16));
-		lblcm.setBounds(907, 338, 74, 31);
+		lblcm.setBounds(852, 358, 74, 31);
 		frame.getContentPane().add(lblcm);
 
 		JButton btnRestablecer = new JButton("Restablecer");
+		JButton btnContinuar = new JButton("Continuar");  // Botón que cierra la primera ventana y abre la segunda
+
+		JLabel lblErrorNombre = new JLabel();
+		lblErrorNombre.setForeground(Color.RED);
+		lblErrorNombre.setFont(new Font("Dialog", Font.ITALIC, 14));
+		lblErrorNombre.setBounds(265, 163, 178, 17);
+		frame.getContentPane().add(lblErrorNombre);
+
+		JLabel lblErrorSexo = new JLabel();
+		lblErrorSexo.setForeground(Color.RED);
+		lblErrorSexo.setFont(new Font("Dialog", Font.ITALIC, 14));
+		lblErrorSexo.setBounds(165, 296, 422, 17);
+		frame.getContentPane().add(lblErrorSexo);
+
+		//ZONA TRABAJO-----------------------------------------------------------
+
+		txtNombre.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtNombre.setText("");
+				txtNombre.setForeground(Color.BLACK);
+				txtNombre.setFont(new Font("Dialog", Font.PLAIN, 16));
+
+			}
+		});
+
 		btnRestablecer.setFont(new Font("Dialog", Font.BOLD, 16));
 		btnRestablecer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -206,36 +226,48 @@ public class DatosUsuario{
 				 * hacemos el clearSelection(); para que no aparezca ningún RadioButton marcado.	
 				 */
 
-
 				txtNombre.setForeground(Color.GRAY);
-				txtNombre.setText("Escribe tu nombre...");				
+				txtNombre.setText(DEFECTO_TXTNOMBRE);
+				txtNombre.setFont(new Font("Dialog", Font.PLAIN, 14));		
 				spinner_anyos.setValue(18);             
 				spinner_peso.setValue(70);
 				spinner_altura.setValue(160);
 				grupoRadioBotones.clearSelection();
+				
+				lblErrorNombre.setText("");
+				lblErrorSexo.setText("");
 
 			}
 		});
-		btnRestablecer.setBounds(121, 433, 288, 27);
+		btnRestablecer.setBounds(121, 453, 288, 27);
 		frame.getContentPane().add(btnRestablecer);
 
-		JButton btnContinuar = new JButton("Continuar");  // Botón que cierra la primera ventana y abre la segunda
+
 		btnContinuar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String sexo = "Hombre";
-				if (rdbtnMujer.isSelected()) 
-					sexo="Mujer";
-				int peso= Integer.parseInt(spinner_peso.getValue().toString()); //obtenemos los valores del usuario para devolverlos mediante el constructor de la otra ventana
-				int altura= Integer.parseInt(spinner_altura.getValue().toString());
-				int edad= Integer.parseInt(spinner_anyos.getValue().toString());
-				Persona usuario = new Persona(txtNombre.getText(), sexo, peso , altura, edad); //Objeto de tipo persona con los datos del usuario
-				frame.setVisible(false); //Se cierra la ventana inicial
-				CalculoNutritivo.arrancar(usuario); //Se arranca la otra ventana pasandole como parametro el objeto de tipo Persona
-				
+				//comprobar que todos los datos se rellenan para poder pasar a la siguiente ventana
+				if (txtNombre.getText().equals("") || txtNombre.getText().equals(DEFECTO_TXTNOMBRE)) {
+					lblErrorNombre.setText("Pon tu nombre porfavor :D");
+				}else if (!rdbtnMujer.isSelected() && !rdbtnHombre.isSelected()) {
+					lblErrorNombre.setText("");
+					lblErrorSexo.setText("Necesitamos que indiques tu sexo para poder hacer los cálculos");
+				}else {
+					lblErrorNombre.setText("");
+					lblErrorSexo.setText("");
+					String sexo = "Hombre";
+					if (rdbtnMujer.isSelected()) 
+						sexo="Mujer";
+					int peso= Integer.parseInt(spinner_peso.getValue().toString()); //obtenemos los valores del usuario para devolverlos mediante el constructor de la otra ventana
+					int altura= Integer.parseInt(spinner_altura.getValue().toString());
+					int edad= Integer.parseInt(spinner_anyos.getValue().toString());
+					Persona usuario = new Persona(txtNombre.getText(), sexo, peso , altura, edad); //Objeto de tipo persona con los datos del usuario
+					frame.setVisible(false); //Se cierra la ventana inicial
+					CalculoNutritivo.arrancar(usuario); //Se arranca la otra ventana pasandole como parametro el objeto de tipo Persona
+				}
 			}
 		});
 		btnContinuar.setFont(new Font("Dialog", Font.BOLD, 16));
-		btnContinuar.setBounds(638, 433, 288, 27);
+		btnContinuar.setBounds(638, 453, 288, 27);
 		frame.getContentPane().add(btnContinuar);
 
 	}
